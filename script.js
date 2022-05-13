394f9d497ed09e391aa83f15bc3cf3bd788d4252
Shubh Gupta <shubh.gupta.0797@gmail.com>
2022-05-14 00:33:25 +0530

    Screen changes according to mobile

--- script.js
+++ script.js
@@ -72,11 +72,11 @@ $(document).ready(function(){
                 items: 1,
                 nav: false
             },
-            600:{
+            800:{
                 items: 2,
                 nav: false
             },
-            1000:{
+            1200:{
                 items: 3,
                 nav: false
             }
